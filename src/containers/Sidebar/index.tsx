9291ef11ff88668da4dc67bbb66a3a31bb7e9052
Bruno Dias <brunodias14319@gmail.com>
2023-10-25 16:15:07 -0300

    construindo a estrtura e a seção "sobre mim"

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/diasbrunodev.png" />
+    <Titulo fontSize={20}>Bruno Dias</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const GithubSecao = styled.div`
+  margin-top: 32px;
+  margin-bottom: 64px;
+
+  img {
+    height: 157px;
+
+    @media (max-width: 768px) {
+      width: 100%;
+      height: 100%;
+      margin-top: 16px;
+      margin-bottom: 8px;
+    }
+  }
+`

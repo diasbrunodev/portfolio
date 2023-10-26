012093fa3976848fc4b0c50a0def345879345dc3
Bruno Dias <brunodias14319@gmail.com>
2023-10-26 11:27:23 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { BotaoTema, Descricao, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/diasbrunodev.png" />
-    <Titulo fontSize={20}>Bruno Dias</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Bruno Dias</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        diasbrunodev
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Front-End
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
